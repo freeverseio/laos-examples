e3bf7f417d64dc6909896c96c986015de1e5c632
Toni <>
2024-12-11 15:22:04 +0100

    using gas estima on evolve

--- update-asset.js
+++ update-asset.js
@@ -7,20 +7,22 @@ require('dotenv').config();
 const { Web3 } = require('web3');
 const axios = require('axios');
 
-// Initialize Web3 instance with LAOS node provider
-const web3 = new Web3('https://rpc.laossigma.laosfoundation.io');
+// Initialize Web3 instance with LAOS node provider. Public nodes:
+// * LAOS Mainnet: https://rpc.laos.laosfoundation.io
+// * LAOS Testnet: https://rpc.laossigma.laosfoundation.io
+const web3 = new Web3('https://rpc.laos.laosfoundation.io');
 
 // Environment variables
 const privateKey = process.env.PRIVATE_KEY;
 
 // The contract address of a collection in LAOS Sigma owned by the sender
-const contractAddress = '0xffFfFfFffFFFfFfFFFffFFfE00000000000000b6';
+const contractAddress = '0xfFFFfffFFffFFFfffffffFFE000000000000010A';
 
 // The IPFS address with the updated metadata of the asset
 const tokenURI = 'ipfs://QmPuwGA4tHHdog5R4w1TUGjVGf2zd1v6fXJZhiXgJ8a1Tj';
 
 // The tokenID of the token to be updated
-const tokenID = '98218211503884169464010162556474302374849222871777179214008520108470801235178';
+const tokenID = '99877789915310102870313747871528163330256470134196093056719091933329985338602';
 
 // The URL of the interface ABI, from GitHub
 const contractABIUrl = 'https://github.com/freeverseio/laos/blob/main/pallets/laos-evolution/src/precompiles/evolution_collection/contracts/EvolutionCollection.json?raw=true';
@@ -35,14 +37,21 @@ async function main() {
     const contract = new web3.eth.Contract(contractABI, contractAddress);
 
     // Prepare the evolve transaction
+    const gasPrice = await web3.eth.getGasPrice();
     const encodedABI = contract.methods.evolveWithExternalURI(tokenID, tokenURI).encodeABI();
     const fromAddress = web3.eth.accounts.privateKeyToAccount(privateKey).address;
+    const gasEstimate = await web3.eth.estimateGas({
+      to: contractAddress,
+      data: encodedABI,
+      from: fromAddress,
+    });
+
     const transaction = {
       from: fromAddress,
       to: contractAddress,
       data: encodedABI,
-      gas: 35000,
-      gasPrice: web3.utils.toWei('0.5', 'gwei'), // Set the desired gas price
+      gas: gasEstimate,
+      gasPrice,
     };
 
     // Sign and send the transaction
@@ -51,8 +60,8 @@ async function main() {
 
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     const blockNumber = await web3.eth.getBlockNumber();
-    console.log('Current block number:', blockNumber);
-    console.log('Transaction confirmed. Asset evolved in block number:', receipt.blockNumber);
+    console.log('Current block number:', Number(blockNumber));
+    console.log('Transaction confirmed. Asset evolved in block number:', Number(receipt.blockNumber));
 
     // Retrieve the token ID from the transaction receipt
     const evolveEventABI = contractABI.find((abi) => abi.name === 'EvolvedWithExternalURI' && abi.type === 'event');
