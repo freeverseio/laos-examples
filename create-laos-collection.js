71c6b5bd1a8ad96eaa66070f1fc12e26c12b0c65
Toni <>
2024-12-13 10:54:07 +0100

    refactoring create collection

--- create-laos-collection.js
+++ create-laos-collection.js
@@ -1,20 +1,21 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-underscore-dangle */
 require('dotenv').config();
 const { ethers } = require('ethers');
 const axios = require('axios');
 
 // Environment variables
-const privateKey = process.env.PRIVATE_KEY;
-if (!privateKey) {
+const { PRIVATE_KEY } = process.env;
+if (!PRIVATE_KEY) {
   throw new Error('Please set PRIVATE_KEY in your .env file.');
 }
 
-// The precompiled contract address exposing collection creation at protocol level:
-const collectionFactoryAddr = '0x0000000000000000000000000000000000000403';
+// Public RPC nodes:
+// - LAOS Mainnet: https://rpc.laos.laosfoundation.io
+// - LAOS Sigma Testnet https://rpc.laossigma.laosfoundation.io
+const PROVIDER_URL = 'https://rpc.laossigma.laosfoundation.io';
 
-// The URL of the interface ABI, from GitHub
-const contractABIUrl = 'https://github.com/freeverseio/laos/blob/main/pallets/laos-evolution/src/precompiles/evolution_collection_factory/contracts/EvolutionCollectionFactory.json?raw=true';
+// The precompiled contract address exposing collection creation at protocol level:
+const COLLECTION_FACTORY_ADDR = '0x0000000000000000000000000000000000000403';
 
 /**
  * Creates a new collection on the LAOS network.
@@ -23,14 +24,13 @@ const contractABIUrl = 'https://github.com/freeverseio/laos/blob/main/pallets/la
  */
 async function createLAOSCollection(wallet) {
   try {
-    // Fetching the contract ABI
-    const response = await axios.get(contractABIUrl);
+    const CONTRACT_ABI_URL = 'https://github.com/freeverseio/laos/blob/main/pallets/laos-evolution/src/precompiles/evolution_collection_factory/contracts/EvolutionCollectionFactory.json?raw=true';
+    const response = await axios.get(CONTRACT_ABI_URL);
     const contractABI = response.data;
 
-    // Initialize the contract
-    const contract = new ethers.Contract(collectionFactoryAddr, contractABI, wallet);
+    const contract = new ethers.Contract(COLLECTION_FACTORY_ADDR, contractABI, wallet);
 
-    console.log('Creating collection...');
+    console.log('Creating sibling collection on LAOS...');
     const tx = await contract.createCollection(wallet.address);
 
     console.log('Transaction sent. Waiting for confirmation...');
@@ -38,28 +38,31 @@ async function createLAOSCollection(wallet) {
 
     console.log('Transaction confirmed. Collection created in block number:', receipt.blockNumber);
 
-    // Retrieve the contract address from the emitted event via the transaction receipt
     const event = receipt.logs.find(
-      (log) => log.address.toLowerCase() === collectionFactoryAddr.toLowerCase(),
+      (log) => log.address.toLowerCase() === COLLECTION_FACTORY_ADDR.toLowerCase(),
     );
     if (event) {
       const iface = new ethers.Interface(contractABI);
       const decodedEvent = iface.decodeEventLog('NewCollection', event.data, event.topics);
-      console.log(`Contract address: ${decodedEvent._collectionAddress}`);
+      console.log(`Collection created at contract address: ${decodedEvent._collectionAddress}`);
       return decodedEvent._collectionAddress;
     }
+
     throw new Error('New collection event log not found.');
   } catch (error) {
-    console.error('Error in createCollection:', error.message || error);
+    console.error('Error in createLAOSCollection:', error.message || error);
     throw error;
   }
 }
 
-// Main function for standalone script execution
+/**
+ * Main function to execute the script.
+ */
 async function main() {
-  const provider = new ethers.JsonRpcProvider('https://rpc.laossigma.laosfoundation.io');
-  const wallet = new ethers.Wallet(privateKey, provider);
+  const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
+  const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
+  console.log('Initializing collection creation...');
   const collectionAddress = await createLAOSCollection(wallet);
   console.log('Collection successfully created at address:', collectionAddress);
 }
