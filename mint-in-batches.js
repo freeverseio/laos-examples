8f3647db60a2b1348b05772d2e4efdbf6691724e
Toni <>
2024-12-13 17:56:43 +0100

    almost completed

--- mint-in-batches.js
+++ mint-in-batches.js
@@ -18,6 +18,18 @@ const PROVIDER_URL = 'https://rpc.laossigma.laosfoundation.io';
 
 const sleep = (seconds) => new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 
+function randomSlot() {
+  return (BigInt(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)) ** 2n) % BigInt(2n ** 96n - 1n);
+}
+
+function randomSlotArray(n) {
+  const result = new Set();
+  while (result.size < n) {
+    result.add(randomSlot()); // Call the function to get its return value
+  }
+  return Array.from(result);
+}
+
 async function deployBatchMinter(wallet) {
   const CONTRACT_ABI_URL = 'https://github.com/freeverseio/laos-smart-contracts/blob/main/batch-minter/artifacts/contracts/LaosBatchMinter.sol/LaosBatchMinter.json?raw=true';
   const response = await axios.get(CONTRACT_ABI_URL);
@@ -34,26 +46,39 @@ async function deployBatchMinter(wallet) {
   console.log('Transaction sent. Waiting for confirmation...');
   await instance.waitForDeployment();
   console.log('Transaction confirmed.');
-  return instance.getAddress();
+  return instance;
 }
 
 async function main() {
-  // const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
-  // const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
-  // console.log('Deployer address:', wallet.address);
-  // const collectionAddress = await deployBatchMinter(wallet);
-  // console.log('BatchMinter successfully deployed at address:', collectionAddress);
+  const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
+  const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+  console.log('Deployer address:', wallet.address);
+  const batchMinter = await deployBatchMinter(wallet);
+  console.log('BatchMinter successfully deployed at address:', await batchMinter.getAddress());
 
   const assets = JSON.parse(fs.readFileSync('mint-in-batches.assets.json', 'utf8'));
-  console.log(assets);
 
   let currentIndex = 0;
+  let nonce = await provider.getTransactionCount(wallet.address);
+  // let nonce = 0;
 
   while (currentIndex < assets.length) {
     const batch = assets.slice(currentIndex, currentIndex + BATCH_SIZE);
-    const nAssetsInBatch = batch.length;
-    console.log(`Processing batch of ${nAssetsInBatch} assets starting at idx = ${currentIndex}`);
+    console.log(`Processing batch with nonce ${nonce}, size ${batch.length}, at idx = ${currentIndex}`);
 
+    const recipients = batch.map((asset) => asset.recipient);
+    console.log(recipients);
+    const uris = batch.map((asset) => asset.tokenURI);
+    console.log(uris);
+    const randoms = randomSlotArray(batch.length);
+    console.log(randoms);
+
+    const tx = await batchMinter.mintWithExternalURIBatch(recipients, randoms, uris, { nonce });
+    console.log(`Transaction with nonce ${nonce} sent: ${tx.hash}`);
+    await tx.wait();
+    console.log(`Transaction with nonce ${nonce} confirmed`);
+
+    nonce += 1;
     currentIndex += BATCH_SIZE;
     await sleep(SECONDS_BETWEEN_SUBMISSIONS);
   }
