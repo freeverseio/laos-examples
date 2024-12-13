e25911153b3a7c755a71048f61488ba29b611396
Toni <>
2024-12-13 10:55:59 +0100

    refactoring

--- create-laos-collection.js
+++ create-laos-collection.js
@@ -14,9 +14,6 @@ if (!PRIVATE_KEY) {
 // - LAOS Sigma Testnet https://rpc.laossigma.laosfoundation.io
 const PROVIDER_URL = 'https://rpc.laossigma.laosfoundation.io';
 
-// The precompiled contract address exposing collection creation at protocol level:
-const COLLECTION_FACTORY_ADDR = '0x0000000000000000000000000000000000000403';
-
 /**
  * Creates a new collection on the LAOS network.
  * @param {ethers.Wallet} wallet - The ethers.js wallet instance.
@@ -28,6 +25,7 @@ async function createLAOSCollection(wallet) {
     const response = await axios.get(CONTRACT_ABI_URL);
     const contractABI = response.data;
 
+    const COLLECTION_FACTORY_ADDR = '0x0000000000000000000000000000000000000403';
     const contract = new ethers.Contract(COLLECTION_FACTORY_ADDR, contractABI, wallet);
 
     console.log('Creating sibling collection on LAOS...');
@@ -44,7 +42,6 @@ async function createLAOSCollection(wallet) {
     if (event) {
       const iface = new ethers.Interface(contractABI);
       const decodedEvent = iface.decodeEventLog('NewCollection', event.data, event.topics);
-      console.log(`Collection created at contract address: ${decodedEvent._collectionAddress}`);
       return decodedEvent._collectionAddress;
     }
 
@@ -55,14 +52,9 @@ async function createLAOSCollection(wallet) {
   }
 }
 
-/**
- * Main function to execute the script.
- */
 async function main() {
   const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
   const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
-
-  console.log('Initializing collection creation...');
   const collectionAddress = await createLAOSCollection(wallet);
   console.log('Collection successfully created at address:', collectionAddress);
 }
