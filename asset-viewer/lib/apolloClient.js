5e5458f9ed2fe8fe1d5f8563c49ea16a163c6110
Toni <>
2025-01-10 11:13:08 +0100

    refactoring

--- asset-viewer/lib/apolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://api.laosnetwork.io/graphql',
-  cache: new InMemoryCache(),
-});
-
-export default client;
