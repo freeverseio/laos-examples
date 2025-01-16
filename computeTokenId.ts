40c9f382079d717827204a09706c2ce357fbb4f9
Toni <>
2025-01-16 08:58:25 +0100

    renaming

--- computeTokenId.ts
+++ computeTokenId.ts
@@ -3,8 +3,8 @@
   as a function of its initial owwner address and a provide random number (slot)
 */
 
-function computeTokenId(user, slot) {
-  if (!/^0x[a-fA-F0-9]{40}$/.test(user)) {
+function computeTokenId(initOwner, slot) {
+  if (!/^0x[a-fA-F0-9]{40}$/.test(initOwner)) {
     throw new Error("Invalid Ethereum address");
   }
   if (slot < 0 || slot >= 2 ** 96) {
@@ -12,7 +12,7 @@ function computeTokenId(user, slot) {
   }
 
   const slotBigInt = BigInt(slot) << 160n;
-  const userBigInt = BigInt(user);
+  const userBigInt = BigInt(initOwner);
   const tokenId = slotBigInt | userBigInt;
   return tokenId;
 }
@@ -20,39 +20,39 @@ function computeTokenId(user, slot) {
 function runTests() {
   const testCases = [
     {
-      user: "0x90abcdef1234567890abcdef1234567890abcdef",
+      initOwner: "0x90abcdef1234567890abcdef1234567890abcdef",
       slot: '0x1234567890abcdef',
       expected: "1917151762750544880654683969214147817878133287987683378847961304559",
       expectedToHex: "0x000000001234567890abcdef90abcdef1234567890abcdef1234567890abcdef",
     },
     {
-      user: "0xffffffffffffffffffffffffffffffffffffffff",
+      initOwner: "0xffffffffffffffffffffffffffffffffffffffff",
       slot: 0,
       expected: "1461501637330902918203684832716283019655932542975",
       expectedToHex: "0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff",
     },
     {
-      user: "0x0000000000000000000000000000000000000000",
+      initOwner: "0x0000000000000000000000000000000000000000",
       slot: 0,
       expected: "0",
       expectedToHex: "0x0000000000000000000000000000000000000000000000000000000000000000",
     },    {
-      user: "0x0000000000000000000000000000000000000001",
+      initOwner: "0x0000000000000000000000000000000000000001",
       slot: 1,
       expected: "1461501637330902918203684832716283019655932542977",
       expectedToHex: "0x0000000000000000000000010000000000000000000000000000000000000001",
     },
     {
-      user: "0x1234567890abcdef1234567890abcdef12345678",
+      initOwner: "0x1234567890abcdef1234567890abcdef12345678",
       slot: '0xffffffffffffffffffff',
       expected: "1766847064778384329583296143170286492852322417545392043886226158472418936",
       expectedToHex:"0x0000ffffffffffffffffffff1234567890abcdef1234567890abcdef12345678",
     },
   ];
 
-  testCases.forEach(({ user, slot, expected, expectedToHex }, index) => {
+  testCases.forEach(({ initOwner, slot, expected, expectedToHex }, index) => {
     try {
-      const result = computeTokenId(user, slot);
+      const result = computeTokenId(initOwner, slot);
       if (result.toString(10) === expected && result ===  BigInt(expectedToHex)) {
         console.log(`Test ${index + 1}: Passed`);
       } else {
